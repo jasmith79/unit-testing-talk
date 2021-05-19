69c4545e739e2bb791d747badba8cf20d0947ab3
jsmith <jasmith79@gmail.com>
2021-05-19 12:59:55 -0400

    Add context file

---
+++ tests/context.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+# We need to insert at the beginning to override the system path
+# lookup. This will enable the tests to import the local packages.
+sys.path.insert(
+    0, 
+    os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)
+
+from some_module2 import not_quite_some_fun, is_valid as better_is_valid
